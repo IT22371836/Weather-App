25567eeae78f59ea26341930deca7e17d91fbf2e
IT22371836 <it22371836@my.sliit.lk>
2025-06-22 12:23:25 +0530

    Update App.jsx
    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Weather from './components/Weather'
+import './Index.css'
 
 const App = () => {
   return (
-    <div classname = 'app'>
-      <Weather/>
+    <div className = 'app'>
+      <Weather/> 
     </div>
+    
+  
   )
 }
 
